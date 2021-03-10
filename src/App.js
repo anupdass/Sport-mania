47353af7ead508837c2c5cde4906bb66874af276
anupdass <anupdas633@gmail.com>
2021-03-10 10:04:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import DisplayTeamList from './components/Header/DisplayTeamList/DisplayTeamList';
+import { useEffect, useState } from 'react';
+import { Tram } from '@material-ui/icons';
 
 function App() {
+  const [team,setteam] = useState([]);
+  useEffect(()=>{
+    const url = 'https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setteam(data.teams) )
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Header></Header>
+         <ul>
+         {
+           team.map(team => <DisplayTeamList team={team}></DisplayTeamList>)
+         }
+         </ul>       
     </div>
   );
 }
